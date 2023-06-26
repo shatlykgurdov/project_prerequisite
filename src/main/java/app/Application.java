4f514b2246b30577f25935a827693c79ca9fa507
Alyona275 <alena.kim.220795@gmail.com>
2023-06-26 16:49:24 +0900

    add task 2.1.4

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,20 @@
+package app;
+
+import app.config.AppConfig;
+import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class App {
+public class Application {
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        for (int i = 0; i < 5; i++) {
+            AnimalsCage bean =
+                    applicationContext.getBean(AnimalsCage.class);
+            bean.whatAnimalSay();
+        }
     }
+
 }
