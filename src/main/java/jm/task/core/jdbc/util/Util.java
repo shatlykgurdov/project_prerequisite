aa3ddb201c1852021ca1af4fe9989f93c54abb90
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-02-02 03:13:14 +0500

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
     // set up a database connection
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/example_schema";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "admin";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
 }
