2abf223344cb7f6df0e4e1575ff1452916581919
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-02-02 03:11:48 +0500

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,49 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+import jm.task.core.jdbc.model.User;
+
+import java.lang.*;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // implement algorithm here
+        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+
+        // Creating a table for Users
+        userDao.createUsersTable();
+
+        // Adding 4 Users to the table with data of your choice
+        userDao.saveUser("Shatlyk", "Gurdov", (byte) 33);
+        System.out.println("User with name - Shatlyk added to the database");
+
+        userDao.saveUser("Zaha", "Badi", (byte) 25);
+        System.out.println("User with name - Zaha added to the database");
+
+        userDao.saveUser("Maksat", "Atayev", (byte) 35);
+        System.out.println("User with name - Maksat added to the database");
+
+        userDao.saveUser("Nury", "Idogdy", (byte) 28);
+        System.out.println("User with name - Nury added to the database");
+
+        // Getting all Users from the database and printing them to the console
+        List<User> users = userDao.getAllUsers();
+        {
+            for (User user : users) {
+                System.out.println(user);
+            }
+        }
+
+        // Removing users by ID from the Users table
+        userDao.removeUserById(1L);
+
+        // Clearing the Users table
+        userDao.cleanUsersTable();
+
+        // Dropping the Users table
+        userDao.dropUsersTable();
     }
 }
