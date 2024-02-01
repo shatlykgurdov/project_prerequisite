c70ef33b67984c1ee4bea4162942ab5916f419c4
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-02-02 03:18:26 +0500

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,28 +4,35 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
+public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl imp = new UserDaoJDBCImpl();
+    public void createUsersTable() {
+        try{
+            imp.createUsersTable();}
+        catch (Exception w){
+
+        }
     }
 
     public void dropUsersTable() {
-
+        imp.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        imp.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        imp.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return imp.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
-    }
+        imp.cleanUsersTable();
+    }   
 }
