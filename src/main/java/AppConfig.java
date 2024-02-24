1d148ff9edb236a43256ab6dc6e8d23fb97e26f6
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-02-24 22:16:20 +0500

    coded as per task requirements
    coded as per task requirements

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
+    @Scope("singleton")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat cat() {
+        return new Cat("Whiskers", 3, "Brown");
     }
 }
