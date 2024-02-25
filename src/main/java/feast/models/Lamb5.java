8c127d737fc2c0b634133f836e5c8009381d5c30
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-02-25 23:19:00 +0500

    Coded as per requirements
    Coded as per requirements

--- src/main/java/feast/models/Lamb5.java
+++ src/main/java/feast/models/Lamb5.java
@@ -1,9 +1,19 @@
 package feast.models;
 
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
 public class Lamb5 {
+    private final Goat6 goat6;
+
+    @Autowired
+    public Lamb5(Goat6 goat6) {
+        this.goat6 = goat6;
+    }
 
     @Override
     public String toString() {
-        return "which is stuffed inside a goat, ";
+        return "which is stuffed inside a goat, " + goat6.toString();
     }
 }
