e88f8c13be599b9d00dbbd9c56f41c3f6377e8a7
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-02-25 22:55:18 +0500

    Completed task
    Completed task

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class Dog extends Animal {
     @Override
     public String toString() {
-        return "Im a Dog";
+        return "I'm a Dog";
     }
 }
