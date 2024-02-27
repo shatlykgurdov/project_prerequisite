e5be6437be260077286ec19dff80f4cd5e44e4ee
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-02-27 23:37:03 +0500

    Coded as per requirements
    Coded as per requirements

--- src/main/java/feast/models/Camel8.java
+++ src/main/java/feast/models/Camel8.java
@@ -3,16 +3,9 @@ package feast.models;
 import org.springframework.stereotype.Component;
 
 @Component
-<<<<<<<< HEAD:src/main/java/feast/models/Dog.java
-public class Dog extends Animal {
-    @Override
-    public String toString() {
-        return "I'm a Dog";
-========
 public class Camel8 {
     @Override
     public String toString() {
         return "and cooked over a charcoal fire.";
->>>>>>>> origin/task_2_1_5:src/main/java/feast/models/Camel8.java
     }
 }
